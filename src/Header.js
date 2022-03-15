d5d7d1807209c0cd894c1d14e8102a5d6b65de2d
linder3hs <linderhassinger00@gmail.com>
2022-03-14 22:37:43 -0500

    react header

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>Inicio</li>
+            <li>Nosotros</li>
+            <li>Contacto</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
