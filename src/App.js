149268b8b618a9e19b75a49cf438e2da4247d1c9
linder3hs <linderhassinger00@gmail.com>
2022-03-14 21:32:21 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// El primer paso para definir un componente es el nombre
+// El nombre de un componente siempre debe iniciar en mayuscula
+// ahora el componente basicamente es una funcion, por ende podemos crearlo
+// usando function o arrow function
+const PrimerComponente = () => {
+  // esta funcion retornar una vista
+  // en react usamos return () para inidicar que lo que este dentro de los
+  // parentesis sera el objeto visual que vamos a retornar
+  // algo importante es que react solo puede retornar un componente a la vez
+  // retorna solo un div
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola mundo</h1>
+      <div>
+        <h4>Hola desde el componente</h4>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+// Luego de crear el componente debemos exportarlo
+// Esto es como decir que este archivo unicamente esta exportando este componete
+export default PrimerComponente;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+// Para poder importar un componente debos llamarlo por el nombre que fue exportado
+import PrimerComponente from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* Para usar un componente debo tratarlo como si fuera una etiqueta HTML */}
+    <PrimerComponente />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
