0405f74820dd7df93d2961ea94ba646f157e9141
linder3hs <linderhassinger00@gmail.com>
2022-03-15 19:43:06 -0500

    react form

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const PrimerComponente = () => {
   // de esta vairable usando useState
   // ojo el valor que va dentro de useState sera el valor inicial de la variable
 
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   // esta funcion retornar una vista
   // en react usamos return () para inidicar que lo que este dentro de los
@@ -33,27 +33,27 @@ const PrimerComponente = () => {
 
   // vamos a crear 2 funciones una para sumar y otra para restar
   // Estas 2 funciones esta cambiado el estado de la variable count
-  const sumar = () => setCount(count + 1);
+  // const sumar = () => setCount(count + 1);
 
-  const restar = () => setCount(count - 1);
+  // const restar = () => setCount(count - 1);
 
-  const parametros = (n1, n2) => {
-    setCount(n1 + n2);
-  };
+  // const parametros = (n1, n2) => {
+  //   setCount(n1 + n2);
+  // };
 
   return (
     <div>
       <Header />
-      <h1>Hola mundo</h1>
+      {/* <h1>Hola mundo</h1> */}
       <div>
-        <h4>Hola desde el componente {count}</h4>
+        {/* <h4>Hola desde el componente {count}</h4> */}
         {/* vamos a crear 2 botones uno para poder sumarle 1 a count y otro para restarle */}
-        <button onClick={sumar}>Sumar</button>
-        <button onClick={restar}>Restar</button>
+        {/* <button onClick={sumar}>Sumar</button> */}
+        {/* <button onClick={restar}>Restar</button> */}
         {/* Si queremos usar parentensis al momento de llamar a una funcion debemos primera
           ejecutarla como un callback */}
         {/* Ademas es la forma en el caso nuestra reciba algun parametro */}
-        <button onClick={() => parametros(10, 20)}>Restar</button>
+        {/* <button onClick={() => parametros(10, 20)}>Restar</button> */}
       </div>
       <hr />
       <FormRegister />
--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,41 @@
+import { useState } from "react";
+
 // Esto sera un componente el cual nos renderizara un formulario de registro
 const FormRegister = () => {
+  // como podemos capturar el valor de un input
+  const [nombre, setNombre] = useState("");
+
+  // En react la forma en la cual obtenemos el valor un input
+  // es usando el onChange este evento nos va a permitir capturar el valor
+  // de los inputs y guardarlos en una variable
+
+  // Recordemos que con event podemos obtener el valor del input
+  // event = event.target.value
+  // vamos a hacer una funcion la cual cada vez que se ejecute el onChange se llame
+  // y modifique el valor de la variable "nombre"
+
+  // la palabra se usa mucho en React porque recordemos que estamos manejando el
+  // estado de la variable
+  // recordemos que event es solo un nombre este puede ser cambiado por ejemplo por e
+  const handleInputName = (e) => {
+    //  que debemos hacer para poder darle el valor nombre?
+    // recordemos que el valor va dentro de los ()
+    setNombre(e.target.value);
+  };
+
   return (
     <div>
       <form action="">
         <h4>Formualario de registro</h4>
+        <h4>Nombre {nombre}</h4>
         <p>
-          <input type="text" placeholder="Ingrese su nombre" />
+          {/* Al ser un input se sobre entiende que el valor que se envia es event 
+          por ende no hace falta pasarle un parametro */}
+          <input
+            type="text"
+            onChange={handleInputName}
+            placeholder="Ingrese su nombre"
+          />
         </p>
         <p>
           <input type="text" placeholder="Ingrese su apellido" />
