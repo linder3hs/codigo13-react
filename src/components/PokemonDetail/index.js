ad332b96fe72153ae639f75df6f9d51dd44cbb57
linder3hs <linderhassinger00@gmail.com>
2022-03-16 21:52:57 -0500

    service

---
+++ src/components/PokemonDetail/index.js
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+const PokemonDetail = () => {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Detalle del pokemon
+      </Button>
+    </div>
+  );
+};
+
+export default PokemonDetail;
