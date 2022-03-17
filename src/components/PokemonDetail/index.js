b4b9d0bc840b3d58b2b3330ef9d74ff5e682d6f3
linder3hs <linderhassinger00@gmail.com>
2022-03-16 22:25:05 -0500

    service

--- src/components/PokemonDetail/index.js
+++ src/components/PokemonDetail/index.js
@@ -1,11 +1,29 @@
-import { Button } from "@mui/material";
+import { useState } from "react";
+import { Button, Dialog, DialogContent } from "@mui/material";
 
 const PokemonDetail = () => {
+  const [abrir, setAbrir] = useState(false);
+
+  // vamos a hacer una funcion la cual se encargue de cambiar el estado
+  const openDialog = () => {
+    setAbrir(true);
+  };
+
+  const closeDialog = () => {
+    setAbrir(false);
+  };
+
   return (
     <div>
-      <Button variant="contained" color="primary">
+      <Button onClick={openDialog} variant="contained" color="primary">
         Detalle del pokemon
       </Button>
+      <Dialog open={abrir} onClose={closeDialog}>
+        <DialogContent>
+          <h4>Pokemon name</h4>
+          <Button onClick={closeDialog}>Cerrar</Button>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
