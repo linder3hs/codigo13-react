aa6ad0d474958264c6abe7fa5ffe8b1d9a02d1d9
linder3hs <linderhassinger00@gmail.com>
2022-03-19 11:03:49 -0500

    react router

--- src/pages/Youtube/index.js
+++ src/pages/Youtube/index.js
@@ -1,109 +1,99 @@
+import { Box, Grid, TextField } from "@mui/material";
+import "./index.css";
+
 const Youtube = () => {
   return (
-    <section class="row container-fluid">
-      <div class="col-md-2 border-right">
-        <div class="m-5">
-          <img
-            src="https://1000marcas.net/wp-content/uploads/2020/02/YouTube-logo.png"
-            class="img-fluid img-logo"
-            alt="logo"
-          />
-          <div class="mt-5">
-            <h6>MENU</h6>
-            <ul>
-              <li>
-                <a href="">
-                  <i class="fa-solid fa-house"></i>&nbsp;Home
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <i class="fa-solid fa-fire"></i>&nbsp;Trending
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <i class="fa-brands fa-youtube"></i>&nbsp;Subscriptions
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <i class="fa-solid fa-podcast"></i>&nbsp;Live
-                </a>
-              </li>
-            </ul>
-            <h6 class="mt-5">LIBRARY</h6>
-            <ul>
-              <li>
-                <a href="">
-                  <i class="fa-solid fa-clock"></i>&nbsp;History
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <i class="fa-solid fa-barcode"></i>&nbsp;Queue
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <i class="fa-solid fa-heart"></i>&nbsp;Likes
-                </a>
-              </li>
-            </ul>
+    <Box m={3}>
+      <Grid container spacing={3}>
+        <Grid item md={2} className="border-right">
+          <div className="m-5">
+            <img
+              src="https://1000marcas.net/wp-content/uploads/2020/02/YouTube-logo.png"
+              className="img-fluid img-logo"
+              alt="logo"
+            />
+            <div className="mt-5">
+              <h6>MENU</h6>
+              <ul>
+                <li>
+                  <a href="">
+                    <i className="fa-solid fa-house"></i>&nbsp;Home
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <i className="fa-solid fa-fire"></i>&nbsp;Trending
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <i className="fa-brands fa-youtube"></i>&nbsp;Subscriptions
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <i className="fa-solid fa-podcast"></i>&nbsp;Live
+                  </a>
+                </li>
+              </ul>
+              <h6 className="mt-5">LIBRARY</h6>
+              <ul>
+                <li>
+                  <a href="">
+                    <i className="fa-solid fa-clock"></i>&nbsp;History
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <i className="fa-solid fa-barcode"></i>&nbsp;Queue
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <i className="fa-solid fa-heart"></i>&nbsp;Likes
+                  </a>
+                </li>
+              </ul>
 
-            <h6 class="mt-5">EXTRA</h6>
-            <ul>
-              <li>
-                <a href="">
-                  <i class="fa-solid fa-circle-question"></i>&nbsp;Settings
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <i class="fa-solid fa-gear"></i>&nbsp;Help
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <i class="fa-solid fa-comments"></i>&nbsp;Feedback
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-10">
-        <div class="row m-5">
-          <div class="col-md-12">
-            <div class="row d-flex align-items-center">
-              <div class="col-md-6">
-                <div class="input-group mb-3">
-                  <span class="input-group-text" id="basic-addon1">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                  </span>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                  />
-                </div>
-              </div>
-              <div class="col-md-6 d-flex justify-content-end">
-                <img
-                  src="https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/people19.png"
-                  class="img-fluid img-profile"
-                  alt=""
-                />
-              </div>
+              <h6 className="mt-5">EXTRA</h6>
+              <ul>
+                <li>
+                  <a href="">
+                    <i className="fa-solid fa-circle-question"></i>
+                    &nbsp;Settings
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <i className="fa-solid fa-gear"></i>&nbsp;Help
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <i className="fa-solid fa-comments"></i>&nbsp;Feedback
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
-
+        </Grid>
+        <Grid item md={10}>
+          <Grid container spacing={3}>
+            <Grid item md={4}>
+              <TextField type="text" label="Username" fullWidth size="small" />
+            </Grid>
+            <Grid item md={8} sx={{ textAlign: "right" }}>
+              <img
+                src="https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/people19.png"
+                className="img-profile"
+                alt=""
+              />
+            </Grid>
+          </Grid>
           <div id="container-videos"></div>
-        </div>
-      </div>
-    </section>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
