b622bb354fdb03bdfdfa432aee16aead0f21a968
linder3hs <linderhassinger00@gmail.com>
2022-03-19 11:33:31 -0500

    react router

--- src/pages/Youtube/index.js
+++ src/pages/Youtube/index.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
 import { Box, Grid, TextField } from "@mui/material";
+import { getMovies } from "../../service/movies";
 import "./index.css";
 
 const Youtube = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const response = await getMovies();
+    setMovies(response);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <Box m={3}>
       <Grid container spacing={3}>
@@ -16,22 +29,22 @@ const Youtube = () => {
               <h6>MENU</h6>
               <ul>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-solid fa-house"></i>&nbsp;Home
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-solid fa-fire"></i>&nbsp;Trending
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-brands fa-youtube"></i>&nbsp;Subscriptions
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-solid fa-podcast"></i>&nbsp;Live
                   </a>
                 </li>
@@ -39,17 +52,17 @@ const Youtube = () => {
               <h6 className="mt-5">LIBRARY</h6>
               <ul>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-solid fa-clock"></i>&nbsp;History
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-solid fa-barcode"></i>&nbsp;Queue
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-solid fa-heart"></i>&nbsp;Likes
                   </a>
                 </li>
@@ -58,18 +71,18 @@ const Youtube = () => {
               <h6 className="mt-5">EXTRA</h6>
               <ul>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-solid fa-circle-question"></i>
                     &nbsp;Settings
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-solid fa-gear"></i>&nbsp;Help
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a href="/">
                     <i className="fa-solid fa-comments"></i>&nbsp;Feedback
                   </a>
                 </li>
@@ -90,7 +103,23 @@ const Youtube = () => {
               />
             </Grid>
           </Grid>
-          <div id="container-videos"></div>
+          {movies.length > 0 &&
+            movies.map((movie) => (
+              <Grid item md={3}>
+                <video
+                  onmouseover="this.play()"
+                  onmouseout="this.pause()"
+                  controls
+                  muted
+                  poster={movie.wallpaper}
+                  class="video"
+                >
+                  <source src={movie.video_link} />
+                </video>
+                <h5 class="title">{movie.name}</h5>
+                <p class="subtitle">{movie.director}</p>
+              </Grid>
+            ))}
         </Grid>
       </Grid>
     </Box>
