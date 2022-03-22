9e87fb5996656b700cac3ed8d31d3aca5b72396e
linder3hs <linderhassinger00@gmail.com>
2022-03-21 21:37:51 -0500

    component update movie

--- src/pages/MovieUpdate/index.js
+++ src/pages/MovieUpdate/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Grid, TextField, Button } from "@mui/material";
-import { getMovieDetail } from "../../service/movies";
+import { Container, Grid, TextField, Button } from "@mui/material";
+import { getMovieDetail, updateMovie } from "../../service/movies";
 
 const MovieUpdate = () => {
   const { id } = useParams();
@@ -35,61 +35,72 @@ const MovieUpdate = () => {
     });
   };
 
+  const fetchUpdateMovie = async () => {
+    await updateMovie(id, values);
+
+    window.location.href = "/youtube/administrador";
+  };
+
   useEffect(() => {
     fetchDetailMovie();
   }, []);
 
   return (
-    <Grid container spacing={3}>
-      <Grid item md={6}>
-        <TextField
-          label="Nombre de la peli"
-          name="name"
-          fullWidth
-          value={values.name}
-          onChange={handleChangeInput}
-        />
+    <Container>
+      <h4>Actualizar Pelicula</h4>
+      <Grid container spacing={3}>
+        <Grid item md={6}>
+          <TextField
+            label="Nombre de la peli"
+            name="name"
+            fullWidth
+            value={values.name}
+            onChange={handleChangeInput}
+          />
+        </Grid>
+        <Grid item md={6}>
+          <TextField
+            label="Nombre del director"
+            name="director"
+            fullWidth
+            value={values.director}
+            onChange={handleChangeInput}
+          />
+        </Grid>
+        <Grid item md={6}>
+          <TextField
+            label="Genero"
+            name="gender"
+            fullWidth
+            value={values.gender}
+            onChange={handleChangeInput}
+          />
+        </Grid>
+        <Grid item md={6}>
+          <TextField
+            label="Link del video"
+            name="video_link"
+            fullWidth
+            value={values.video_link}
+            onChange={handleChangeInput}
+          />
+        </Grid>
+        <Grid item md={6}>
+          <TextField
+            label="Link de la portada"
+            name="wallpaper"
+            fullWidth
+            value={values.wallpaper}
+            onChange={handleChangeInput}
+          />
+        </Grid>
+        <Grid item md={12}>
+          <Button variant="contained" onClick={fetchUpdateMovie}>
+            Actualizar
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item md={6}>
-        <TextField
-          label="Nombre del director"
-          name="director"
-          fullWidth
-          value={values.director}
-          onChange={handleChangeInput}
-        />
-      </Grid>
-      <Grid item md={6}>
-        <TextField
-          label="Genero"
-          name="gender"
-          fullWidth
-          value={values.gender}
-          onChange={handleChangeInput}
-        />
-      </Grid>
-      <Grid item md={6}>
-        <TextField
-          label="Link del video"
-          name="video_link"
-          fullWidth
-          value={values.video_link}
-          onChange={handleChangeInput}
-        />
-      </Grid>
-      <Grid item md={6}>
-        <TextField
-          label="Link de la portada"
-          name="wallpaper"
-          fullWidth
-          value={values.wallpaper}
-          onChange={handleChangeInput}
-        />
-      </Grid>
-      <Grid item md={12}>
-        <Button variant="contained">Crear</Button>
-      </Grid>
-    </Grid>
+    </Container>
   );
 };
 
