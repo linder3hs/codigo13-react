c81be307dbf9995914665e7640f2022070dfa95f
linder3hs <linderhassinger00@gmail.com>
2022-03-21 19:45:46 -0500

    component create movie

--- src/components/MovieCreate/index.js
+++ src/components/MovieCreate/index.js
@@ -4,6 +4,23 @@ import { Button, Dialog, DialogContent, TextField, Grid } from "@mui/material";
 const MovieCreate = () => {
   const [open, setOpen] = useState(false);
 
+  const [values, setValues] = useState({
+    name: "",
+    director: "",
+    gender: "",
+    video_link: "",
+    wallpaper: "",
+  });
+
+  const handleChangeInput = (e) => {
+    const { value, name } = e.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
   const handleOpenDialog = () => {
     setOpen(!open);
   };
@@ -17,26 +34,38 @@ const MovieCreate = () => {
         <DialogContent>
           <Grid container spacing={3}>
             <Grid item md={6}>
-              <TextField label="Nombre de la peli" name="name" fullWidth />
+              <TextField
+                label="Nombre de la peli"
+                name="name"
+                fullWidth
+                onChange={handleChangeInput}
+              />
             </Grid>
             <Grid item md={6}>
               <TextField
                 label="Nombre del director"
                 name="director"
                 fullWidth
+                onChange={handleChangeInput}
               />
             </Grid>
             <Grid item md={6}>
               <TextField label="Genero" name="gender" fullWidth />
             </Grid>
             <Grid item md={6}>
-              <TextField label="Link del video" name="video_link" fullWidth />
+              <TextField
+                label="Link del video"
+                name="video_link"
+                fullWidth
+                onChange={handleChangeInput}
+              />
             </Grid>
             <Grid item md={6}>
               <TextField
                 label="Link de la portada"
                 name="wallpaper"
                 fullWidth
+                onChange={handleChangeInput}
               />
             </Grid>
             <Grid item md={12}>
