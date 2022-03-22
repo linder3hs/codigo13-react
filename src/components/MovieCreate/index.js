d3901de3d212164ccc212bbd577d41b46fbc949c
linder3hs <linderhassinger00@gmail.com>
2022-03-21 19:27:35 -0500

    component create movie

--- src/components/MovieCreate/index.js
+++ src/components/MovieCreate/index.js
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialgContent, TextField, Grid } from "@mui/material";
+import { Button, Dialog, DialogContent, TextField, Grid } from "@mui/material";
 
 const MovieCreate = () => {
   return (
@@ -6,6 +6,38 @@ const MovieCreate = () => {
       <Button variant="contained" color="primary">
         Crear Pelicula
       </Button>
+      <Dialog open>
+        <DialogContent>
+          <Grid container spacing={3}>
+            <Grid item md={6}>
+              <TextField label="Nombre de la peli" name="name" fullWidth />
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                label="Nombre del director"
+                name="director"
+                fullWidth
+              />
+            </Grid>
+            <Grid item md={6}>
+              <TextField label="Genero" name="gender" fullWidth />
+            </Grid>
+            <Grid item md={6}>
+              <TextField label="Link del video" name="video_link" fullWidth />
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                label="Link de la portada"
+                name="wallpaper"
+                fullWidth
+              />
+            </Grid>
+            <Grid item md={12}>
+              <Button variant="contained">Crear</Button>
+            </Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
