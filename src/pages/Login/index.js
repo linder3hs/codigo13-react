f5b3021ed82bccf6b50228c215b7ab0d78799942
linder3hs <linderhassinger00@gmail.com>
2022-03-22 20:49:19 -0500

    login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,15 +3,42 @@ import bgLogin from "../../assets/bg-login.png";
 
 const Login = () => {
   return (
-    <Grid container spacing={3} alignItems="center" sx={{ height: "100vh" }}>
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="space-around"
+      sx={{ height: "100vh", padding: 20, backgroundColor: "#FFD885" }}
+    >
       <Grid item md={6}>
         <img src={bgLogin} width={600} alt="" />
       </Grid>
       <Grid item md={6}>
-        <Card>
+        <Card sx={{ width: 500 }}>
           <CardContent>
             <h5>Welcome to</h5>
             <h3>Tecsup - Codigo</h3>
+            <p>
+              Bienvenido a la comunidad de CodiGo, juntos aprenderemos a
+              programas paginas web.
+            </p>
+            <Grid container spacing={3} mt={5}>
+              <Grid item md={12}>
+                <TextField label="Email" fullWidth />
+              </Grid>
+              <Grid item md={12}>
+                <TextField label="Password" fullWidth />
+              </Grid>
+              <Grid item md={12}>
+                <Button
+                  sx={{ backgroundColor: "#000" }}
+                  variant="contained"
+                  fullWidth
+                  size="large"
+                >
+                  Iniciar Session
+                </Button>
+              </Grid>
+            </Grid>
           </CardContent>
         </Card>
       </Grid>
