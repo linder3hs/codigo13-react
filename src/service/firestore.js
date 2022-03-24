2a88d2a4f0bf0e27924ac949462280bb3562fb0f
linder3hs <linderhassinger00@gmail.com>
2022-03-23 21:35:20 -0500

    firebase

---
+++ src/service/firestore.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4O5g7loVbjNvJKw0KRb1eH9N_aTca5Tw",
+  authDomain: "codigo13-34b65.firebaseapp.com",
+  projectId: "codigo13-34b65",
+  storageBucket: "codigo13-34b65.appspot.com",
+  messagingSenderId: "436776363555",
+  appId: "1:436776363555:web:129d19c2cbe95e9ca4f396",
+  measurementId: "G-9H7HFQ7NS0",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
