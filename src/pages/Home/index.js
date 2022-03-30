5aa58b5e217a2f1735a700e643b6f1a52ec06f89
linder3hs <linderhassinger00@gmail.com>
2022-03-29 22:23:05 -0500

    add home chart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,6 +13,35 @@ const Home = () => {
       xaxis: {
         categories: ["JS", "CSS", "HTML", "Firebase", "NodeJS", "React", "Vue"],
       },
+      fill: {
+        colors: ["#fffaaa"],
+      },
+      grid: {
+        row: {
+          colors: ["#F44336", "#E91E63", "#9C27B0"],
+        },
+        column: {
+          colors: ["#F44336", "#E91E63", "#9C27B0"],
+        },
+      },
+      chart: {
+        zoom: {
+          enabled: true,
+          type: "x",
+          autoScaleYaxis: false,
+          zoomedArea: {
+            fill: {
+              color: "#90CAF9",
+              opacity: 0.4,
+            },
+            stroke: {
+              color: "#0D47A1",
+              opacity: 0.4,
+              width: 1,
+            },
+          },
+        },
+      },
     },
   };
 
@@ -35,10 +64,18 @@ const Home = () => {
         <Chart
           options={{
             labels: ["A", "B", "C", "D", "E"],
+            fill: {
+              colors: ["#addccc", "#f01", "#F44336", "#E91E63", "#ccaadd"],
+            },
+            dataLabels: {
+              style: {
+                colors: ["#addccc", "#f01", "#F44336", "#E91E63", "#ccaadd"],
+              },
+            },
           }}
           series={[44, 55, 41, 17, 15]}
           type="donut"
-          width={300}
+          width={500}
         />
       </Grid>
     </Container>
