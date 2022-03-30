a3885724129957794eb6e3dd608b83e5b4f6fd2f
linder3hs <linderhassinger00@gmail.com>
2022-03-29 19:43:26 -0500

    add layout admin

--- src/layouts/Private/index.js
+++ src/layouts/Private/index.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { UserContext } from "../../Context/UserContext";
 import { Navigate, Outlet, Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 
 const Private = () => {
   const { user } = useContext(UserContext);
@@ -13,12 +13,85 @@ const Private = () => {
   }
 
   return (
-    <>
-      <Link to="/perfil">
-        <Button variant="outlined">Perfil</Button>
-      </Link>
-      <Outlet />
-    </>
+    <Box m={3}>
+      <Grid container spacing={3}>
+        <Grid item md={1.5} className="border-right">
+          <div className="m-5">
+            <img
+              src="https://1000marcas.net/wp-content/uploads/2020/02/YouTube-logo.png"
+              className="img-fluid img-logo"
+              alt="logo"
+            />
+            <div className="mt-5">
+              <h6>MENU</h6>
+              <ul>
+                <li>
+                  <a href="/">
+                    <i className="fa-solid fa-house"></i>&nbsp;Home
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i className="fa-solid fa-fire"></i>&nbsp;Trending
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i className="fa-brands fa-youtube"></i>&nbsp;Subscriptions
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i className="fa-solid fa-podcast"></i>&nbsp;Live
+                  </a>
+                </li>
+              </ul>
+              <h6 className="mt-5">LIBRARY</h6>
+              <ul>
+                <li>
+                  <a href="/">
+                    <i className="fa-solid fa-clock"></i>&nbsp;History
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i className="fa-solid fa-barcode"></i>&nbsp;Queue
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i className="fa-solid fa-heart"></i>&nbsp;Likes
+                  </a>
+                </li>
+              </ul>
+
+              <h6 className="mt-5">EXTRA</h6>
+              <ul>
+                <li>
+                  <a href="/">
+                    <i className="fa-solid fa-circle-question"></i>
+                    &nbsp;Settings
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i className="fa-solid fa-gear"></i>&nbsp;Help
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i className="fa-solid fa-comments"></i>&nbsp;Feedback
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Grid>
+        <Grid item md={10}>
+          <Outlet />
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
