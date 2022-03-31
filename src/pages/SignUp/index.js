93f851abc549adb454ff82f8708d4df4c3428b79
linder3hs <linderhassinger00@gmail.com>
2022-03-30 22:26:06 -0500

    Sign Up view

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,7 @@
 import { Container, Grid, TextField } from "@mui/material";
 import { useFormik } from "formik";
+import DateAdapter from "@mui/lab/AdapterDateFns";
+import { LocalizationProvider, DatePicker } from "@mui/lab";
 
 // nombre
 // apellido
@@ -51,7 +53,21 @@ const SignUp = () => {
           <Grid item md={6} xs={12}>
             <TextField label="Ciudad" name="city" fullWidth />
           </Grid>
-          <Grid item md={6} xs={12}></Grid>
+          <Grid item md={6} xs={12}>
+            <LocalizationProvider dateAdapter={DateAdapter}>
+              <DatePicker
+                label="Fecha de nacimiento"
+                name="date_born"
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            {/* <TextField
+              label="Fecha de nacimiento"
+              name="date_born"
+              type="date"
+              fullWidth
+            /> */}
+          </Grid>
           <Grid item md={6} xs={12}></Grid>
           <Grid item md={6} xs={12}></Grid>
         </Grid>
