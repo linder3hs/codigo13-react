d3017a617a83a387ade35b653403318270f8fa20
linder3hs <linderhassinger00@gmail.com>
2022-03-30 21:26:49 -0500

    map view

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,5 +1,12 @@
+import { useState } from "react";
 import { Container, Grid } from "@mui/material";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
@@ -11,6 +18,50 @@ const Map = () => {
     iconUrl: require("../../assets/marker.png"),
     iconSize: [30, 30],
   });
+
+  // vamos a crear una lista de lugares
+  const lugares = [
+    {
+      nombre: "Parque de las aguas",
+      coordenadas: [-12.0943025, -77.0048953],
+    },
+    {
+      nombre: " Palacio de la Exposición",
+      coordenadas: [-12.0599468, -77.0368597],
+    },
+    {
+      nombre: "parque de la amistad",
+      coordenadas: [-12.131846296219146, -76.98250034501207],
+    },
+    {
+      nombre: "zoologico de huachipa",
+      coordenadas: [-12.015033, -76.9004835],
+    },
+    {
+      nombre: "Museo de Arte Contemporáneo - Lima",
+      coordenadas: [-12.1370749, -77.0238592],
+    },
+  ];
+
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        console.log(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position} icon={markerIcon}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  }
 
   return (
     <Container maxWidth="lg">
@@ -24,12 +75,18 @@ const Map = () => {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker
-              position={[-12.045312465453938, -76.95167653769764]}
+            {/* <Marker
+              position={[-12.043973616974938, -76.95295478638475]}
               icon={markerIcon}
             >
               <Popup>Tecsup Centro Educativo</Popup>
-            </Marker>
+            </Marker> */}
+            {lugares.map((lugar) => (
+              <Marker position={lugar.coordenadas} icon={markerIcon}>
+                <Popup>{lugar.nombre}</Popup>
+              </Marker>
+            ))}
+            <LocationMarker />
           </MapContainer>
         </Grid>
       </Grid>
