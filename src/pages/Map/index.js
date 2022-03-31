3bbd1c4334e12cc1c50840c5033d498ae6c8525a
linder3hs <linderhassinger00@gmail.com>
2022-03-30 20:34:20 -0500

    map view

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,10 +1,16 @@
 import { Container, Grid } from "@mui/material";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 const Map = () => {
   // latitude y longitede de lima
   const position = [-12.04318, -77.02824];
+
+  const markerIcon = new L.icon({
+    iconUrl: require("../../assets/marker.png"),
+    iconSize: [30, 30],
+  });
 
   return (
     <Container maxWidth="lg">
@@ -13,11 +19,17 @@ const Map = () => {
           <h1>Mapas</h1>
         </Grid>
         <Grid item md={12}>
-          <MapContainer center={position} zoom={12} style={{ height: 500 }}>
+          <MapContainer center={position} zoom={13} style={{ height: 500 }}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <Marker
+              position={[-12.045312465453938, -76.95167653769764]}
+              icon={markerIcon}
+            >
+              <Popup>Tecsup Centro Educativo</Popup>
+            </Marker>
           </MapContainer>
         </Grid>
       </Grid>
